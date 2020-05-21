5646dafbc044faea0dfddc1bd483cad7bb4f6564
Nathan Chan <NChan@nbbj.com>
2020-05-21 15:25:31 -0700

    completed algos course

--- Ch06/06_02/Begin/LinearSearch/Program.cs
+++ Ch06/06_02/Begin/LinearSearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace LinearSearch
 {
@@ -6,7 +7,31 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            //TODO
+            int theValue = 7;
+            int[] array = new int[] { 1, 4, 5, 7, 9, 22 };
+
+            WriteLine("Our array contains:");
+            Array.ForEach(array, x => Write(x + " "));
+
+            Write($"\n\nThe result of a linear search " +
+                $"for {theValue} is: ");
+            WriteLine(linearSearch(array, theValue));
+        }
+
+        static int linearSearch(int[] a, int x)
+        {
+
+            // Step 2
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == x)
+                {
+                    return i;
+                }
+            }
+
+            // Step 3
+            return -1;
         }
     }
 }
